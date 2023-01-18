fabfacfa3541573cc83d2dfdb96d0b4bed90e3f7
Illia Marchenko <lxgiwylaezakmi298@gmail.com>
2023-01-18 17:39:53 +0200

    stage2

--- cgi-bin/rest.py
+++ cgi-bin/rest.py
@@ -15,7 +15,9 @@ obj['meter']['gas']['reading'] = random.randint(10000,100000)/10
 obj['meter']['gas']['consumption'] = random.randint(1,20)/10
 obj['meter']['water']['reading'] = random.randint(10000,100000)/10
 obj['meter']['water']['consumption'] = random.randint(1,20)/10
-# obj['boiler']['isRun'] = 
+
+#  obj['boiler']['isRun'] - здесь рандом неуместен, тк пользователь сам устанавливает значение 
+
 obj['boiler']['temperature'] = random.randint(60, 80)
 obj['boiler']['pressure'] = random.randint(10,20)/10
 
--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import threading, time, json, requests, sys
 
 json_response = ''
 lock_json_response = threading.Lock()
-event_json_response = threading.Event()
+event_exit = threading.Event()
 
 def request():
     global json_response
-    if event_json_response.is_set():
-        return
-    response = requests.get('http://localhost:8000/cgi-bin/rest.py')
-    lock_json_response.acquire()
-    json_response = json.loads(response.text)
-    time.sleep(5)
-    lock_json_response.release()
+    while True:
+        if event_exit.is_set():
+            return
+        response = requests.get('http://localhost:8000/cgi-bin/rest.py')
+        lock_json_response.acquire()
+        json_response = json.loads(response.text)
+        lock_json_response.release()
+        time.sleep(5)
 
 def menu():
     while True:
-        choise = input('''Меню:
+        choise = input('''\nМеню:
 1. Температура/Влажность
     1.1 Текущая # последняя запись в файле
     1.2 Средняя # среднее 6 последних записей
@@ -37,17 +38,72 @@ def menu():
             humidity = json_response['humidity']
             lock_json_response.acquire()
             with open('result.txt', 'a', encoding='utf-8') as f:
-                f.write(f'{time.asctime()} Температура - {temperature}, Давление - {humidity}')
+                f.write(f'\n{time.asctime()} Температура - {temperature}, Давление - {humidity}')
             lock_json_response.release()
             print(f'Температура - {temperature}, Давление - {humidity}')
         elif choise == '2.1':
             el_reading = json_response['meter']['electricity']['reading']
             el_consumption = json_response['meter']['electricity']['consumption']
+            lock_json_response.acquire()
             with open('result.txt', 'a', encoding='utf-8') as f:
-                f.write(f'{time.asctime()} ЭЛЕКТРОЕНЕРГИЯ - Показание счётчика - {el_reading}, Текущий расход - {el_consumption}')
+                f.write(f'\n{time.asctime()} ЭЛЕКТРОЕНЕРГИЯ - Показание счётчика - {el_reading}, Текущий расход - {el_consumption}')
             lock_json_response.release()
             print(f'ЭЛЕКТРОЕНЕРГИЯ - Показание счётчика - {el_reading}, Текущий расход - {el_consumption}')
         elif choise == '2.2':
             gas_reading = json_response['meter']['gas']['reading']
             gas_consumption = json_response['meter']['gas']['consumption']
-                
+            lock_json_response.acquire()
+            with open('result.txt', 'a', encoding='utf-8') as f:
+                f.write(f'\n{time.asctime()} ГАЗ - Показание счётчика - {gas_reading}, Текущий расход - {gas_consumption}')
+            lock_json_response.release()
+            print(f'ГАЗ - Показание счётчика - {gas_reading}, Текущий расход - {gas_consumption}')
+        elif choise == '2.3':
+            water_reading = json_response['meter']['water']['reading']
+            water_consumption = json_response['meter']['water']['consumption']
+            lock_json_response.acquire()
+            with open('result.txt', 'a', encoding='utf-8') as f:
+                f.write(f'\n{time.asctime()} ВОДА - Показание счётчика - {water_reading}, Текущий расход - {water_consumption}')
+            lock_json_response.release()
+            print(f'ВОДА - Показание счётчика - {water_reading}, Текущий расход - {water_consumption}')
+        elif choise == '3.1':
+            boiler_status = json_response['boiler']['isRun']
+            boiler_temperature = json_response['boiler']['temperature']
+            boiler_pressure = json_response['boiler']['pressure']
+            
+            if boiler_status == True:    #проверка бойлера на включённость
+                boiler_status_str = 'Бойлер включён'
+            else:
+                boiler_status_str = 'Бойлер выключен'
+            
+            lock_json_response.acquire()
+            with open('result.txt', 'a', encoding='utf-8') as f:
+                f.write(f'\n{time.asctime()} {boiler_status_str}, Температура - {boiler_temperature}, Давление - {boiler_pressure}')
+            lock_json_response.release()
+            print(f'{boiler_status_str}, Температура - {boiler_temperature}, Давление - {boiler_pressure}')
+
+        elif choise == '3.2':
+            boiler_status = True
+            lock_json_response.acquire()
+            with open('result.txt', 'a', encoding='utf-8') as f:
+                f.write(f'\n{time.asctime()} Бойлер включён')
+            lock_json_response.release()
+
+        elif choise == '3.3':
+            boiler_status = False
+            lock_json_response.acquire()
+            with open('result.txt', 'a', encoding='utf-8') as f:
+                f.write(f'\n{time.asctime()} Бойлер выключён')
+            lock_json_response.release()
+        
+        elif choise == "5":
+            sys.exit()
+        event_exit.set()
+
+th1 = threading.Thread(target=menu)
+th2 = threading.Thread(target=request, daemon=True)
+
+th1.start()
+th2.start()
+
+th1.join()
+th2.join()
